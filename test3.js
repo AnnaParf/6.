c568534c88a509ef76f2568f0f589f1d58b743a0
AnnaParf <90256161+AnnaParf@users.noreply.github.com>
2021-12-21 16:32:25 +0200

    Add files via upload

---
+++ test3.js
@@ -0,0 +1,14 @@
+btn/addEventListener("click", fnc);
+functoin  fnc() { 
+    console.log(а.checked); 
+    console.log(б.checked); 
+    console.log(в.checked); 
+    console.log(г.checked);
+    console.log(д.checked);
+    if (б.checked) { 
+        alert ("Правильно");
+    } else { 
+        alert("Помилка");
+    }
+
+}
